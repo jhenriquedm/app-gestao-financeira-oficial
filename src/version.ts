8a1e1c289f1be348c8ea2b397e3e245b078e2002
jhenriquedm <jhenriquedm98@gmail.com>
2026-09-17 20:25:03 +0000

    ci: add SHA-1 check step in build-apk.yml and bump version to 1.3.2

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.3.1';
-export const APP_BUILD_NUMBER = 23;
+export const APP_VERSION = '1.3.2';
+export const APP_BUILD_NUMBER = 24;
 export const APP_RELEASE_DATE = '17/09/2026';
