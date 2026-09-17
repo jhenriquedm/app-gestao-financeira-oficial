7313e16d6897a1da6d96cd339f935e9d5976f0c2
jhenriquedm <jhenriquedm98@gmail.com>
2026-09-17 12:42:11 +0000

    fix(npm): add .npmrc with legacy-peer-deps to fix CI build ERESOLVE error and bump version to 1.2.3

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.2.2';
-export const APP_BUILD_NUMBER = 14;
+export const APP_VERSION = '1.2.3';
+export const APP_BUILD_NUMBER = 15;
 export const APP_RELEASE_DATE = '17/09/2026';
