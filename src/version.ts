d7ab8ec277b9830bb4d8744a5503fa814a1756a1
jhenriquedm <jhenriquedm98@gmail.com>
2026-09-17 12:07:27 +0000

    bump(version): update application version to 1.2.1

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.2.0';
-export const APP_BUILD_NUMBER = 12;
+export const APP_VERSION = '1.2.1';
+export const APP_BUILD_NUMBER = 13;
 export const APP_RELEASE_DATE = '17/09/2026';
