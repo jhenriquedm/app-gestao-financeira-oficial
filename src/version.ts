e964a844dca99e3d487364cca51c1bf99134c317
jhenriquedm <jhenriquedm98@gmail.com>
2026-09-18 12:49:25 +0000

    fix(android): disable allowBackup and update version to 1.4.0 (build 32)

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '1.3.9';
-export const APP_BUILD_NUMBER = 31;
+export const APP_VERSION = '1.4.0';
+export const APP_BUILD_NUMBER = 32;
 export const APP_RELEASE_DATE = '18/09/2026';
